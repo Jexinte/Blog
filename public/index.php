294232262f5fa90a497ea34c0099f4ba38feee97
Yokke <mdembelepro@gmail.com>
2023-07-17 19:06:50 +0200

    refactor : minor changes

--- public/index.php
+++ public/index.php
@@ -213,28 +213,28 @@ if (isset($_GET['action'])) {
         case "update_article":
 
 
-     $defautValuesInEachField =
-     [
-         "title" => $_POST["title"],
-         "short_phrase" => $_POST["short_phrase"],
-         "content" => $_POST["content"],
-         "tags" => $_POST["tags"],
-         "file_image" => $_POST["original_file_path"],
-         "id_article" => $_POST['id_article']
-        ];
-        
-        $template = "admin_update_article.twig";
-        $paramaters = [
-            "message" => $articleController->handleUpdateArticleValidator($_POST["title"], $_FILES["image_file"],$_POST["original_file_path"], $_POST["short_phrase"], $_POST["content"], $_POST["tags"], $_SESSION,$_POST["id_article"]),
-            "default_value" => $defautValuesInEachField,
-            
-        ];
-   
-        
+            $defautValuesInEachField =
+                [
+                    "title" => $_POST["title"],
+                    "short_phrase" => $_POST["short_phrase"],
+                    "content" => $_POST["content"],
+                    "tags" => $_POST["tags"],
+                    "file_image" => $_POST["original_file_path"],
+                    "id_article" => $_POST['id_article']
+                ];
+
+            $template = "admin_update_article.twig";
+            $paramaters = [
+                "message" => $articleController->handleUpdateArticleValidator($_POST["title"], $_FILES["image_file"], $_POST["original_file_path"], $_POST["short_phrase"], $_POST["content"], $_POST["tags"], $_SESSION, $_POST["id_article"]),
+                "default_value" => $defautValuesInEachField,
+
+            ];
+
+
             break;
-            case "delete_article":
-                $articleController->handleDeleteArticle($_GET["id"],$_SESSION);
-                break;
+        case "delete_article":
+            $articleController->handleDeleteArticle($_GET["id"], $_SESSION);
+            break;
         case "logout":
             $logout = $userController->handleLogout($_SESSION);
             if (is_array($logout) && array_key_exists("logout", $logout) && $logout["logout"] == 1) $sessionRepository->destroySession();
@@ -288,12 +288,12 @@ if (isset($_GET['action'])) {
             $paramaters["session"] =  $_SESSION;
             break;
         case "view_update_article":
-           
+
             $template = "admin_update_article.twig";
             $paramaters = [
-                "article" => $articleController->handleOneArticle($_GET["id"])[0],
+                "article" => current($articleController->handleOneArticle($_GET["id"])),
             ];
-     
+
             break;
     }
 }
