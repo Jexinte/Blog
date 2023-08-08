6959dc42d74c6eb5b777f852500f03ec396b3731
Yokke <mdembelepro@gmail.com>
2023-08-08 16:06:38 +0200

    refactor : object type is replace by the name of the object

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -60,7 +60,7 @@ class ArticleController
   }
 
 
-  public function handleTextField(string $keyArray, string $value, string $keyException, object $exception, string $regex, string $emptyException, string $wrongFormatException): string|array
+  public function handleTextField(string $keyArray, string $value, string $keyException, ValidationException $exception, string $regex, string $emptyException, string $wrongFormatException): string|array
   {
     switch (true) {
       case empty($value):
