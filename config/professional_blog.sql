1dd1a7c3e8bd6f46d048e13c672490b7f2ee1aed
Yokke <mdembelepro@gmail.com>
2023-08-15 09:06:25 +0200

    update

--- config/professional_blog.sql
+++ config/professional_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 15 août 2023 à 08:26
+-- Généré le : mar. 15 août 2023 à 09:04
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -24,6 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `chapô` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `dateCreation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id`, `image`, `title`, `chapô`, `content`, `tags`, `author`, `dateCreation`) VALUES
+(1, 'http://localhost/P5_Créez votre premier blog en PHP - Dembele Mamadou/public/assets/images/banner_article/article_1.jpg', 'PHP : Introduction au langage de programmation web', 'Découvrez les bases de PHP', 'PHP est un langage de programmation largement utilisé pour développer des sites web dynamiques. Dans cet article, nous allons explorer les concepts de base de PHP, y compris les variables, les boucles et les fonctions.', '#PHP #WebDevelopment #Programming', 'Admin', '2023-07-24'),
+(2, 'http://localhost/P5_Créez votre premier blog en PHP - Dembele Mamadou/public/assets/images/banner_article/article_2.jpg', 'Les meilleures pratiques de sécurité en PHP', 'Protégez vos applications web avec PHP', 'La sécurité est essentielle lors du développement d\'applications web avec PHP. Découvrez les meilleures pratiques pour protéger votre code contre les failles courantes telles que les injections SQL et les attaques par cross-site scripting (XSS).', '#PHP #Security #WebDevelopment', 'Admin', '2023-07-24'),
+(3, 'http://localhost/P5_Créez votre premier blog en PHP - Dembele Mamadou/public/assets/images/banner_article/article_3.jpg', 'PHP : Gérer les formulaires et les données utilisateur', 'Apprenez à manipuler les données des utilisateurs', 'Les formulaires sont couramment utilisés pour collecter des données auprès des utilisateurs. Dans cet article, nous allons explorer comment traiter et valider les données envoyées via des formulaires en utilisant PHP.\r\nLes formulaires sont couramment utilisés pour collecter des données auprès des utilisateurs. Dans cet article, nous allons explorer comment traiter et valider les données envoyées via des formulaires en utilisant PHP.', '#PHP #GérerFormulaire #ValidationDeDonnées', 'Admin', '2023-08-03');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `idArticle` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `dateCreation` date NOT NULL,
+  `feedbackAdministrator` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `form_message`
+--
+
+CREATE TABLE `form_message` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `profileImage` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` enum('user','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `user_notification`
 --
 
@@ -40,6 +112,32 @@ CREATE TABLE `user_notification` (
 --
 
 --
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idArticle` (`idArticle`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Index pour la table `form_message`
+--
+ALTER TABLE `form_message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `user_notification`
 --
 ALTER TABLE `user_notification`
@@ -52,6 +150,30 @@ ALTER TABLE `user_notification`
 --
 
 --
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `form_message`
+--
+ALTER TABLE `form_message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `user_notification`
 --
 ALTER TABLE `user_notification`
@@ -60,6 +182,13 @@ ALTER TABLE `user_notification`
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`idArticle`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `user_notification`
